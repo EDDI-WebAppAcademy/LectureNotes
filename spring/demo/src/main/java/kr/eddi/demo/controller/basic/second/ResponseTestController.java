989cf54acd9b24b79c6350dc0bf58f5c949550e4
silenc3502 <gcccompil3r@gmail.com>
2022-07-21 21:22:30 +0900

    [2회차] RequestMapping, PathVariable, Lombok(Getter, Setter), ResponseBody 테스트

---
+++ spring/demo/src/main/java/kr/eddi/demo/controller/basic/second/ResponseTestController.java
@@ -0,0 +1,28 @@
+package kr.eddi.demo.controller.basic.second;
+
+import kr.eddi.demo.entity.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+@RequestMapping("/second/response")
+public class ResponseTestController {
+
+    // ResponseBody 리턴 결과를 자동으로 json 형태로 처리함
+    // 그러므로 Controller에서도 json 데이터를 전송할 수 있다.
+    @ResponseBody
+    @GetMapping("/member")
+    public Member responseMember() {
+        log.info("responseMember() 동작");
+
+        Member member = new Member();
+        member.setUserName("내맘대로할거야");
+        member.setPassword("싫어!!!");
+
+        return member;
+    }
+}
---
+++ spring/demo/src/main/java/kr/eddi/demo/entity/Member.java
@@ -0,0 +1,15 @@
+package kr.eddi.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+// 위의 Getter, Setter가 실제 Lombok에서 제공하는 자동화 기능임.
+// 클래스 내부의 변수들을 보고 알아서 Getter와 Setter를 만듬
+// 그러나 경우에 따라 커스텀 매서드를 만들어야 할 수도 있으므로
+// 실제로 매서드를 만드는 방법에 대해서는 잘 숙지하고 있어야함
+public class Member {
+    private String userName = "eddi";
+    private String password = "456123";
+}
