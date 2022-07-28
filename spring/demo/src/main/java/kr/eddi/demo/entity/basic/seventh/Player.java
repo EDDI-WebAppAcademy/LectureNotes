5ec231c54a7123a72da8f607862d27dd58909a9b
silenc3502 <gcccompil3r@gmail.com>
2022-07-28 19:58:35 +0900

    feat: 주사위 게임 Collection 기반 작업 완료 + 일부 주석 추가 [DICE-GAMEn]

--- spring/demo/src/main/java/kr/eddi/demo/entity/basic/seventh/Player.java
+++ spring/demo/src/main/java/kr/eddi/demo/entity/basic/seventh/Player.java
@@ -34,6 +34,14 @@ public class Player implements Comparable<Player> {
         }
     }
 
+    // 코드 단위로 분석하면 대가리 폭발하므로
+    // 쉽게 생각한다면 아래와 같은 메커니즘으로 보도록 한다.
+    // 1. 내가 비교하고자 하는 정보가 무엇인가 ? 현재 우리케이스는 totalScore
+    //    그래서 비교 대상에 this.getScore().getTotalScore(),
+    //    player.getScore().getTotalScore()이 사용됨
+    // 2. 다음으로 한쪽이 크면 양수, 작으면 음수, 같으면 0으로 배치하면
+    //    내가 비교하고자 하는 값으로 언제든지 정렬이됨
+    // 3. 내부 코드 볼 필요 없음 (추상화)
     @Override
     public int compareTo(Player player) {
         int srcScore = this.getScore().getTotalScore();
