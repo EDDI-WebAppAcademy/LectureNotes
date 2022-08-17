c8cb9c9babc795a6db8feac56d4afcbde946fa27
silenc3502 <gcccompil3r@gmail.com>
2022-08-17 15:55:22 +0900

    feat: 19th - json 응답 [ENTITY-RESn]

--- spring/demo/src/main/java/kr/eddi/demo/controller/basic/nineteenth/EntityResponseController.java
+++ spring/demo/src/main/java/kr/eddi/demo/controller/basic/nineteenth/EntityResponseController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/19th")
@@ -16,5 +19,19 @@ public class EntityResponseController {
         Member member = new Member();
 
         return member;
+    }
+
+    @GetMapping("/member-list-response")
+    public List<Member> memberListResponse() {
+        List<Member> memberList = new ArrayList<>();
+
+        Member member1 = new Member();
+        memberList.add(member1);
+
+        Member member2 = new Member();
+        member2.setUserName("test-account");
+        memberList.add(member2);
+
+        return memberList;
     }
 }
