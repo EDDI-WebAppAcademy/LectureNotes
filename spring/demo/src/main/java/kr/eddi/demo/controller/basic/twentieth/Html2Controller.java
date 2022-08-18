8ab6bc02d3381c10cdd1aa415845303adddac4bb
silenc3502 <gcccompil3r@gmail.com>
2022-08-18 16:10:30 +0900

    feat: 20th - HTML/CSS Basics 2 [HTML-CSS-BASICn]

---
+++ spring/demo/src/main/java/kr/eddi/demo/controller/basic/twentieth/Html2Controller.java
@@ -0,0 +1,33 @@
+package kr.eddi.demo.controller.basic.twentieth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/20th")
+public class Html2Controller {
+
+    @GetMapping("/img-size-test")
+    public String imgSizeTest() {
+        log.info(("img-size-test"));
+
+        return "basic/twentieth/img-size-test";
+    }
+
+    @GetMapping("/img-caption-test")
+    public String imgCaptionTest() {
+        log.info(("img-caption-test"));
+
+        return "basic/twentieth/img-caption-test";
+    }
+
+    @GetMapping("/display-method-test")
+    public String displayMethodTest() {
+        log.info(("display-method-test"));
+
+        return "basic/twentieth/display-method-test";
+    }
+}
