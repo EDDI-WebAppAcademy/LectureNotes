0aa07a2696fd79d7450605c4c1518ebbd58295e8
Sanghoon Lee <gcccompil3r@gmail.com>
2022-08-24 16:56:12 +0900

    vue 기본 설정 이후 기본기 I [24회차]

---
+++ vue/frontend/src/components/basics/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ testMsg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Test",
+  data() {
+    return {
+      testMsg: "My Message",
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Test/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Test from "@/components/basics/Test";
+
+export default {
+  name: 'HomeView',
+  components: {
+    Test
+  }
+}
+</script>
