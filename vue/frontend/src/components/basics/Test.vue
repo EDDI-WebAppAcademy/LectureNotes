e8fa75e5c3ab371eb5c784409823b48c2a338377
silenc3502 <gcccompil3r@gmail.com>
2022-08-25 20:03:02 +0900

    vue로 rpg 게임 만들어보기 I [25회차]

--- vue/frontend/src/components/basics/Test.vue
+++ vue/frontend/src/components/basics/Test.vue
@@ -20,7 +20,9 @@
 
     <!-- 현재 컨텐츠 측면에서 몬스터가 매번 같은것이 추가되서 재미 요소가 반감됨(그러므로 랜덤 요소를 넣어야함) -->
     (고정)몬스터 이름: <input v-model="name">
-    <button v-on:click="addFixedMonster">고정 몬스터 추가하기</button>
+    <button v-on:click="addFixedMonster">고정 몬스터 추가하기</button><br/>
+
+    <button v-on:click="addRandomMonster">랜덤 몬스터 추가하기</button><br/>
 
     <ul>
       <li v-for="(monster, index) in monsterLists" :key="index">
@@ -44,6 +46,29 @@ export default {
       show: true,
       num: 1,
       count: 0,
+      monsterBooks: [
+        { monsterId: 1, name: '슬라임', hp: 50, exp: 10, dropMoney: 5 },
+        { monsterId: 2, name: '고블린', hp: 100, exp: 20, dropMoney: 10 },
+        { monsterId: 3, name: '놀', hp: 200, exp: 40, dropMoney: 20 },
+        { monsterId: 4, name: '멧돼지', hp: 400, exp: 100, dropMoney: 20 },
+        { monsterId: 5, name: '스켈레톤', hp: 350, exp: 90, dropMoney: 10 },
+        { monsterId: 6, name: '오크', hp: 550, exp: 150, dropMoney: 40 },
+        { monsterId: 7, name: '홉고블린', hp: 650, exp: 180, dropMoney: 50 },
+        { monsterId: 8, name: '스켈레톤 아처', hp: 300, exp: 150, dropMoney: 50 },
+        { monsterId: 9, name: '트롤', hp: 800, exp: 200, dropMoney: 80 },
+        { monsterId: 10, name: '좀비', hp: 350, exp: 90, dropMoney: 50 },
+        { monsterId: 11, name: '고블린 매지션', hp: 700, exp: 500, dropMoney: 300 },
+        { monsterId: 12, name: '오우거', hp: 1000, exp: 250, dropMoney: 50 },
+        { monsterId: 13, name: '고스트', hp: 750, exp: 300, dropMoney: 1 },
+        { monsterId: 14, name: '골렘', hp: 2000, exp: 400, dropMoney: 100 },
+        { monsterId: 15, name: '와이번', hp: 3500, exp: 500, dropMoney: 150 },
+        { monsterId: 16, name: '리치', hp: 2000, exp: 1000, dropMoney: 500 },
+        { monsterId: 17, name: '스켈레톤 기사', hp: 2500, exp: 700, dropMoney: 100 },
+        { monsterId: 18, name: '웨어울프', hp: 5000, exp: 1000, dropMoney: 700 },
+        { monsterId: 19, name: '미노타우루스', hp: 10000, exp: 1500, dropMoney: 1000 },
+        { monsterId: 20, name: '드레이크', hp: 20000, exp: 5000, dropMoney: 50000 },
+        { monsterId: 21, name: '죽음의 군주', hp: 1000000, exp: 200000, dropMoney: 1000000 },
+      ],
       monsterLists: [
         { id: 1, name: '슬라임', hp: 50 },
         { id: 2, name: '고블린', hp: 100 },
@@ -60,12 +85,15 @@ export default {
     increment () {
       this.count += 1
     },
+    findCurrentMonsterListMax() {
+      return this.monsterLists.reduce(
+          (a, b) => { console.log("a: " + a + ", b.id: " + b.id); return a > b.id ? a : b.id },
+          0)
+    },
     addFixedMonster () {
       // let은 javascript에서 사용하는 변수 개념입니다.
       // java에서 Object와 유사
-      let max = this.monsterLists.reduce(
-          (a, b) => { console.log("a: " + a + ", b.id: " + b.id); return a > b.id ? a : b.id },
-          0)
+      let max = this.findCurrentMonsterListMax()
 
       this.monsterLists.push({
         id: max + 1,
@@ -78,6 +106,18 @@ export default {
       // 리스트에서 index 파트를 특정 개수만큼 잘라낸다.
       // splice(index, 1)이므로 index에 해당하는 정보 1개를 삭제한다라는 뜻입니다.
       this.monsterLists.splice(index, 1)
+    },
+    addRandomMonster () {
+      let max = this.findCurrentMonsterListMax()
+
+      // ex) 20개라면 0 ~ 19.xxx 까지인데 floor 버림이니까 0 ~ 19까지
+      let randomMonsterBookIdx = Math.floor(Math.random() * this.monsterBooks.length)
+
+      this.monsterLists.push({
+        id: max + 1,
+        name: this.monsterBooks[randomMonsterBookIdx].name,
+        hp: this.monsterBooks[randomMonsterBookIdx].hp
+      })
     }
   }
 }
